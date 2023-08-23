e9b7c7fec184720c2daf8451b7340f29aa117f3f
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-08-23 13:52:18 -0500

    3 - Fetching Data with useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,26 @@ import './App.css'
 import Navbar from './components/Navbar'
 import HomePage from './components/HomePage'
 import SuperheroesPage from './components/SuperheroesPage'
+import { QueryClientProvider, QueryClient } from "react-query"
 import RQSuperheroesPage from './components/RQSuperheroesPage'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <section className="container py-5">
-        <Routes>
-          <Route path="/super-heroes" element={ <SuperheroesPage /> } />
-          <Route path="/rq-super-heroes" element={ <RQSuperheroesPage /> } />
-          <Route path="/" element={ <HomePage /> } />
-        </Routes>
-      </section>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Navbar />
+        <section className="container py-5">
+          <Routes>
+            <Route path="/super-heroes" element={<SuperheroesPage />} />
+            <Route path="/rq-super-heroes" element={<RQSuperheroesPage />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </section> 
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
--- src/components/RQSuperheroesPage.tsx
+++ src/components/RQSuperheroesPage.tsx
@@ -1,6 +1,24 @@
+import axios from "axios"
+import { useQuery } from "react-query"
+
+const fetchSuperHeroes = () => axios.get("http://localhost:4000/superheroes");
+
 const RQSuperheroesPage = () => {
+  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
+
   return (
-    <div>RQSuperheroesPage</div>
+    <article>
+      <h2>Super Heroes Page</h2>
+      <ul>
+        {data?.data.map((hero) => {
+          return <li key={hero["id"]}>{hero["name"]}</li>
+        })}
+      </ul>
+    </article>
   )
 }
 
